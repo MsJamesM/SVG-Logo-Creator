f9168047d16b88fe3e21db20305683f14eb2b72f
MsJamesM <118479305+MsJamesM@users.noreply.github.com>
2023-06-19 13:39:06 -0600

    added content to shapes.js file. Connected contents in index.js to shapes.js. Added fs.writefile function

--- index.js
+++ index.js
@@ -1,29 +1,65 @@
 // packages needed for application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const shapes = require("./lib/shapes");
+const { Circle, Square, Triangle } = require("./lib/shapes.js");
 
 // creates an array of questions for user input
-inquirer.prompt([
-  {
-    type: "input",
-    name: "text",
-    message: "Choose three letters for your logo",
-  },
-  {
-    type: "input",
-    name: "textColor",
-    message: "Choose a color for the text",
-  },
-  {
-    type: "input",
-    name: "shape",
-    message: "Choose a shape for the logo",
-    choices: ["Circle", "Square", "Triangle"],
-  },
-  {
-    type: "list",
-    name: "shapeColor",
-    message: "Choose a color for the shape",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "text",
+      message: "Choose three letters for your logo",
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "Choose a color for the logo text",
+    },
+    {
+      type: "list",
+      name: "shapes",
+      message: "Choose a shape for the logo",
+      choices: ["Circle", "Square", "Triangle"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "Choose a color for the shape",
+    },
+  ])
+  .then((logoOutput) => {
+    let shapes;
+
+    if (logoOutput.shapes === "Circle") {
+      shapes = new Circle(
+        logoOutput.shapeColor,
+        logoOutput.text,
+        logoOutput.textColor
+      );
+    }
+
+    if (logoOutput.shapes === "Square") {
+      shapes = new Square(
+        logoOutput.shapeColor,
+        logoOutput.text,
+        logoOutput.textColor
+      );
+    }
+
+    if (logoOutput.shapes === "Triangle") {
+      shapes = new Triangle(
+        logoOutput.shapeColor,
+        logoOutput.text,
+        logoOutput.textColor
+      );
+    }
+
+    fs.writeFile("./examples/logo.svg", shapes.render(), (err) => {
+      if (err) {
+        console.log("❌ ㅤError - please try again ❌");
+      } else {
+        console.log("✔️ ㅤYour logo has been created ✔️");
+      }
+    });
+  });
